4fda4d6e176b8bc8ae6d81a8d956263eb4dec5b5
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-03-15 19:33:10 +0530

    finite-difference time-domain simulation

---
+++ src/fdtd/simulation.c
@@ -0,0 +1,46 @@
+/* FDTD simulation */
+
+#include <string.h>
+#include "sources.h"
+
+#define pulse(...) pulse((struct pulse){__VA_ARGS__});
+#define sinusoidal(...) sinusoidal((struct sinusoidal){__VA_ARGS__});
+
+/**
+ * Finite-difference time-domain method
+ *
+ * :param int ke: number of electric and magnetic field nodes
+ * :param double *ex: electric field oriented in the x direction
+ * :param double *hy: magnetic field oriented in the y direction
+ *
+ * :return: *Ex, *Hy: stack of electric and magnetic field
+ */
+int simulate(int ke, double *ex, double *hy, double *Ex, double *Hy) {
+
+    int nsteps = 500;
+
+    double lbound[2] = {0, 0};
+    double hbound[2] = {0, 0};
+
+    /* FDTD simulation loop */
+    for (int time_step = 1; time_step <= nsteps; time_step++) {
+
+	    /* calculate the Ex field */
+	    for (int k = 1; k < ke; ex[k] = ex[k] + 0.5 * (hy[k-1] - hy[k]), k++);
+
+	    /* sinusoidal wave source (frequency 1900 MHz) */
+	    ex[1] = ex[1] + sinusoidal(time_step, .freq = 1900e6);
+
+	    /* absorbing boundary conditions */
+	    ex[0] = lbound[0], lbound[0] = lbound[1], lbound[1] = ex[1];
+	    ex[ke-1] = hbound[0], hbound[0] = hbound[1], hbound[1] = ex[ke-2];
+
+	    /* calculate the Hy field */
+	    for (int k = 0; k < ke - 1; hy[k] = hy[k] + 0.5 * (ex[k] - ex[k+1]), k++);
+
+	    memcpy(Ex + (time_step - 1) * ke, ex, ke * sizeof(*ex));
+	    memcpy(Hy + (time_step - 1) * ke, hy, ke * sizeof(*hy));
+    }
+
+    return 0;
+}
